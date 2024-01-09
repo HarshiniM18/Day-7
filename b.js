25bcd592c2c4e6e2ec75a7f7b722cea557d34782
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-01-09 20:40:06 +0530

    b.js

--- b.js
+++ b.js
@@ -4,7 +4,7 @@ request.send();
 request.onload = function(){
     var result = JSON.parse(request.response);
     console.log(result);
-    var a = result.filter((ele)=>ele.population>200000)
+    var a = result.filter((ele)=>ele.population<200000)
     console.log(a)
     var b = a.map((ele)=>ele.name)
     console.log(b)
