0d898b0c253efb634a1f179a86046787c730485c
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-05-14 17:32:33 +0530

    b.js

--- b.js
+++ b.js
@@ -1,9 +1,11 @@
-fetch('https://restcountries.com/v3.1/all')
-  .then(response => response.json())
-  .then(data => {
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    const result = JSON.parse(request.response);
+    console.log(result);
     
     // Get all the countries with a population of less than 2 lakhs using Filter method
-    const countriesWithPopulationLessThan2Lakhs = data.filter(country => country.population < 200000);
+    const countriesWithPopulationLessThan2Lakhs = result.filter(country => country.population < 200000);
     console.log('Countries with population less than 2 lakhs:', countriesWithPopulationLessThan2Lakhs);
-
-   
+}
