e8a3e4b2484ae16269527960d1d98979768fda03
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-05-14 17:20:11 +0530

    a.js

--- a.js
+++ a.js
@@ -2,10 +2,10 @@ var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function(){
-    var result = JSON.parse(request.response);
+    const result = JSON.parse(request.response);
     console.log(result);
-    var res = result.filter((ele)=>ele.continents=="Asia")
-    console.log(res)
-    var c = res.map((ele)=>ele.name)
-    console.log(c)
+    const res = result.filter((ele) => ele.continent === "Asia");
+    console.log(res);
+    const c = res.map((ele) => ele.name.common);
+    console.log(c);
 }
