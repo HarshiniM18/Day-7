0bd7a65a2a24fdd171e493caf736eaeaaabbf9ac
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-05-14 17:23:28 +0530

    d.js

--- d.js
+++ d.js
@@ -2,8 +2,8 @@ var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function(){
-    var result = JSON.parse(request.response);
+    const result = JSON.parse(request.response);
     console.log(result);
-    var res = result.reduce((acc, ele)=> acc+ele.population, 0);
-    console.log("Total population",res)
+    const totalPopulation = result.reduce((acc, country) => acc + country.population, 0);
+    console.log("Total population:", totalPopulation);
 }
